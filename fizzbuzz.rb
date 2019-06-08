089db1eaa3ccd6d8d1e20a266d085531940bdf87
John Jordan Shelley <jshelley553@gmail.com>
2019-06-08 02:37:02 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,11 @@
-def fizzbuzz
-
-
-
+def fizzbuzz(num)
+  if num % 3 == 0 
+    return "fizz"
+  end 
+  if num % 5 == 0 
+    return "buzz"
+  end 
+  if num % 3 == 0 || num % 5 == 0 
+    return "fizzbuzz"
+  end  
 end 
